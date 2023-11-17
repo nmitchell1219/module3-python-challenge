ddcdd3a96cddff82aa3236fd3a101229e7830565
Natalia Mitchell <nataliamitchell19@gmail.com>
2023-11-16 21:50:36 -0600

    Final revision for puthon challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,28 +1,69 @@
 #In this Challenge, you are tasked with creating a Python script to analyze the financial records of your company. 
 #You will be given a financial dataset called `budget_data.csv`. The dataset is composed of two columns: "Date" and "Profit/Losses".
-
-csv_file_path = 'C:\Users\natal\OneDrive\Desktop\Module Challenges\Module 3 - Python\python-challenge\PyBank\Resources\budget_data.csv'
-
-with open(csv_file_path, newline=',') as csvfile:
-    csv_reader = csv.reader(csvfile)
-    
-
-#Your task is to create a Python script that analyzes the records to calculate each of the following values:
-
 #* The total number of months included in the dataset
-  header = next(csv_reader, None)
-  total_months = sum(1 for row in csv_reader)
+
 #* The net total amount of "Profit/Losses" over the entire period
-
 #* The changes in "Profit/Losses" over the entire period, and then the average of those changes
-
 #* The greatest increase in profits (date and amount) over the entire period
-
 #* The greatest decrease in profits (date and amount) over the entire period
+import os
+import csv
+
+budget_csv = os.path.join(os.path.dirname(__file__), "Resources", "budget_data.csv")
+
+total_months = 0
+total_profit_losses = 0
+previous_profit_loss = 0
+profit_loss_changes = []
+months = []
+
+with open(budget_csv, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    
+    next(csv_reader)
+
+    for row in csv_reader:
+        total_months += 1
+
+        total_profit_losses += int(row[1])
+
+        if total_months > 1:
+            profit_loss_change = int(row[1]) - previous_profit_loss
+            profit_loss_changes.append(profit_loss_change)
+            months.append(row[0])
+
+        previous_profit_loss = int(row[1])
+
+average_change = sum(profit_loss_changes) / len(profit_loss_changes)
+
+greatest_increase = max(profit_loss_changes)
+greatest_increase_month = months[profit_loss_changes.index(greatest_increase)]
+
+greatest_decrease = min(profit_loss_changes)
+greatest_decrease_month = months[profit_loss_changes.index(greatest_decrease)]
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${total_profit_losses}")
+print(f"Average Change: ${round(average_change, 2)}")
+print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
+
+text_file = "financial_analysis.txt"
+with open(text_file, 'w') as file:
+    file.write("Financial Analysis\n")
+    file.write("----------------------------\n")
+    file.write(f"Total Months: {total_months}\n")
+    file.write(f"Total: ${total_profit_losses}\n")
+    file.write(f"Average Change: ${round(average_change, 2)}\n")
+    file.write(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})\n")
+    file.write(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})\n")
+
+print(f"Results exported to {text_file}")
 
 #Your analysis should align with the following results:
 
-#```text
 #Financial Analysis
 #----------------------------
 #Total Months: 86
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,28 +5,78 @@
 #You will be given a set of poll data called `election_data.csv`. The dataset is composed of three columns: "Voter ID", "County", and "Candidate". Your task is to create a Python script that analyzes the votes and calculates each of the following values:
 
 #* The total number of votes cast
-
 #* A complete list of candidates who received votes
-
 #* The percentage of votes each candidate won
-
 #* The total number of votes each candidate won
-
 #* The winner of the election based on popular vote
+
+total_votes = 0
+candidates = {}
+
+import os
+import csv
+
+election_data_csv = os.path.join(os.path.dirname(__file__), "Resources", "election_data.csv")
+
+with open(election_data_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    
+    next(csvreader, None)
+       
+    for row in csvreader:
+        voter_id, county, candidate = row
+
+        total_votes += 1
+
+        if candidate in candidates:
+            candidates[candidate]["votes"] += 1
+        else:
+            candidates[candidate] = {"votes": 1}
+
+
+winner_name = max(candidates, key=lambda x: candidates[x]["votes"])
+winner_votes = candidates[winner_name]["votes"]
+
+
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+for candidate, data in candidates.items():
+    percentage = (data["votes"] / total_votes) * 100
+    print(f"{candidate}: {percentage:.3f}% ({data['votes']})")
+print("-------------------------")
+print(f"Winner: {winner_name}")
+print("-------------------------")
+
+output_file_path = "election_results.txt"
+with open(output_file_path, "w") as output_file:
+    output_file.write("Election Results\n")
+    output_file.write("-------------------------\n")
+    output_file.write(f"Total Votes: {total_votes}\n")
+    output_file.write("-------------------------\n")
+    for candidate, data in candidates.items():
+        percentage = (data["votes"] / total_votes) * 100
+        output_file.write(f"{candidate}: {percentage:.3f}% ({data['votes']})\n")
+    output_file.write("-------------------------\n")
+    output_file.write(f"Winner: {winner_name}\n")
+    output_file.write("-------------------------\n")
+
+print(f"Election results have been printed to the terminal and saved to '{output_file_path}'.")
 
 #Your analysis should align with the following results:
 
 #```text
 #Election Results
--------------------------
+#-------------------------
 #Total Votes: 369711
--------------------------
+#-------------------------
 #Charles Casper Stockham: 23.049% (85213)
 #Diana DeGette: 73.812% (272892)
 #Raymon Anthony Doane: 3.139% (11606)
--------------------------
+#-------------------------
 #Winner: Diana DeGette
--------------------------
-```
+#-------------------------
+#```
 
 #In addition, your final script should both print the analysis to the terminal and export a text file with the results.
